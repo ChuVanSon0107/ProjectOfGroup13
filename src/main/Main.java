ed4ec74bf5fbd52ee93f6a1adf14f642f84dc9e0
Chu Van Son <cvson1126034@gmail.com>
2024-11-05 17:37:14 +0700

    game

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import resources.Loader;
+
+public class Main {
+    public static void main(String[] args){
+        Loader loader = new Loader();
+        loader.load();
+        Engine.init();
+        Engine.start();     
+    }
+}
+
