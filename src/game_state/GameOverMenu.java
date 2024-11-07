3d3da231807720acd5e0468febe838ed54d040da
Chu Van Son <cvson1126034@gmail.com>
2024-11-07 19:18:57 +0700

    update pause and game menu

--- src/game_state/GameOverMenu.java
+++ src/game_state/GameOverMenu.java
@@ -9,36 +9,42 @@ import resources.Resources;
 public class GameOverMenu extends MainMenu{
 	public static boolean result ;
 	protected String[] gameoverMenu = {};
-	private static final String WIN = "You win";
-	private static final String LOSE = "You lose";
-	private static final String RESTART = "Restart";
-	private static final String MAINMENU = "Main menu";
+	private static final String WIN = "YOU WIN";
+	private static final String LOSE = "YOU LOSE";
+	private static final String RESTART = "RESTART";
+	private static final String MAINMENU = "MAIN MENU";
 	protected int selected;
 	public GameOverMenu(GameStateManager gameStateManager){
 		super(gameStateManager);
 		this.gameoverMenu = new String[]{WIN, LOSE};
 		this.optionsMenu = new String[]{RESTART, MAINMENU};
-		this.selected= 0;
+		this.selected = 0;
 	}
 
 	
 
 	@Override
 	public void Loop() {
-		// TODO Auto-generated method stub
+
 		
 	}
 
 	@Override
 	public void Render(Graphics graphics) {
-		// TODO Auto-generated method stub
+
 		graphics.drawImage(Resources.TEXTURES.get(Resources.BACKGROUND),0, 0, WindowManager.WIDTH, WindowManager.HEIGHT, null);
 		
 		graphics.setFont(new Font("Arial", Font.BOLD, 40));
 		graphics.drawImage(Resources.TEXTURES.get(Resources.BUTTON), 0, 0, 800, 200, null);
-		graphics.setColor(Color.WHITE);
-		int index;if(result)index = 0; else index = 1;
-		graphics.drawString(this.gameoverMenu[index], WindowManager.WIDTH/2 - 115, WindowManager.HEIGHT/5);
+		
+		graphics.setColor(Color.BLACK);
+		if(result == true){
+			graphics.drawString(this.gameoverMenu[0], 300, 120);
+		}
+		else{
+			graphics.drawString(this.gameoverMenu[1], 300, 120);
+		}
+
 		for (int i = 0; i < this.optionsMenu.length; i++) {
 			graphics.drawImage(Resources.TEXTURES.get(Resources.BUTTON_1), 0, 180 + 80 * i, 400, 80, null);
 			if(i == this.selected) {
@@ -47,14 +53,13 @@ public class GameOverMenu extends MainMenu{
 			}
 			else graphics.setColor(Color.WHITE);
 			
-			graphics.drawString(this.optionsMenu[i], 10, 235 + i * 80);
+			graphics.drawString(this.optionsMenu[i], 50, 235 + i * 80);
 		}
 		
 	}
 
 	@Override
 	public void KeyPressed(int keyCode) {
-		// TODO Auto-generated method stub
 		switch (keyCode){
 			case KeyEvent.VK_UP:
 				if(this.selected > 0) this.selected --;
--- src/game_state/PauseMenu.java
+++ src/game_state/PauseMenu.java
@@ -32,8 +32,8 @@ public class PauseMenu extends MainMenu{
 		
 		graphics.setFont(new Font("Arial", Font.BOLD, 40));
 		graphics.drawImage(Resources.TEXTURES.get(Resources.BUTTON), 0, 0, 800, 200, null);
-		graphics.setColor(Color.WHITE);
-		graphics.drawString(LABEL, WindowManager.WIDTH/2 - 115, WindowManager.HEIGHT/5);
+		graphics.setColor(Color.BLACK);
+		graphics.drawString(LABEL, 340, 120);
 		for (int i = 0; i < this.optionsMenu.length; i++) {
 			graphics.drawImage(Resources.TEXTURES.get(Resources.BUTTON_1), 0, 180 + 80 * i, 400, 80, null);
 			if(i == this.selected) {
@@ -42,7 +42,7 @@ public class PauseMenu extends MainMenu{
 			}
 			else graphics.setColor(Color.WHITE);
 			
-			graphics.drawString(this.optionsMenu[i], 10, 235 + i * 80);
+			graphics.drawString(this.optionsMenu[i], 50, 235 + i * 80);
 		}
 	}
 	@Override
