ead38851f5112cc373cd977794a46dd61d06369b
Vũ Quang Thắng <thangbienhoa3gmail.com@MacBook-Pro-cua-Vu.local>
2024-11-07 10:48:47 +0700

    change index and change map

---
+++ Downloads/ProjectOfGroup13-GuideMenu/src/game_world/Tile.java
@@ -0,0 +1,38 @@
+package game_world;
+
+import java.awt.Rectangle;
+
+import resources.Resources;
+
+public class Tile extends Rectangle{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private byte id;
+	public static final int size = 50;
+	private byte prop;
+	// 0: None
+	// 1: Wall
+	// 2: Water
+	public Tile(byte id,int x,int y) {
+		super(x * size, y * size, size, size);
+		this.id = id;
+		// prop = 0 -> pass
+		// prop = 1 -> block
+		// prop = 2 -> slow
+		// prop = 3 -> change room 
+		if(id== Resources.WALL || id==Resources.STONE || id== Resources.TREE)prop=1;
+		else if(id >= Resources.GRASS1 && id <= Resources.GRASS4)prop = 1;
+		else if(id >=Resources.WALL1 && id<=Resources.WALL21)prop = 0;
+		else if(id==Resources.WATER || id==Resources.DIRT)prop=2;
+		else if(id >= Resources.CAVE1 || id <= Resources.CAVE6) prop = 3;
+		else prop=0;
+	}
+	public byte GetProperty() {
+		return prop;
+	}
+	public byte getID() {
+		return id;
+	}
+}
