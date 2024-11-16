70a31a340dcad7602cd1f33aec95ea90b27b413c
thangbienhoa3 <thangbienhoa3@gmail.com>
2024-11-16 17:23:37 +0700

    test

---
+++ Desktop/ProjectOfGroup13/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import resources.Loader;
+
+public class Main {
+    public static void main(String[] args){
+        Loader loader = new Loader();
+        loader.load();
+        Engine.init();
+        Engine.start();     
+    }
+}
+
+
+
+
+
+
